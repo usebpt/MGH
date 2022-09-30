7209869f9d15fe5a63afb95ba0c027d91b9e4c79
usebpt <hoanghon.trenrung@yahoo.com>
2022-09-30 15:37:57 +0700

    second test how to push an updat eto remote repo

--- hello_git.py
+++ hello_git.py
@@ -6,6 +6,16 @@ from lib import python_hub
 
 
 def hello_git():
+    """
+    the func hello_git() to learn how to work with git/github
+
+    param: 
+        None
+
+    return:
+        None
+    """
+
     print (__file__)
     python_hub.list_path_in_sys()
 
