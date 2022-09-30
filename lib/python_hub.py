cefe2c9993ae9a2624fb01f721a2f82a9b990f8f
usebpt <hoanghon.trenrung@yahoo.com>
2022-09-30 15:29:24 +0700

    add description for func

--- lib/python_hub.py
+++ lib/python_hub.py
@@ -1,8 +1,19 @@
 import sys
 
 
-
+#-----------------------------------------------------------------------------
+# 
 def list_path_in_sys(print_out=1):
+    """
+    func list_path_in_sys(print_out=1)
+        get list of path that it is include into sys
+
+    param:
+        print_out is 1 if we need to print info to screen
+
+    return 
+        Array(str)
+    """
     print (__file__)
     for p in sys.path:
         if print_out:
